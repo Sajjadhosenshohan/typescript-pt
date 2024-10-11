b8177106b162a9e026568783711bce387a25ff0e
shohan15020 <mdshohansajjad@gmail.com>
2024-10-11 21:32:12 +0600

    typescript boolean type

---
+++ index.js
@@ -0,0 +1,9 @@
+// function sum(a:number,b:number):number{
+//     return a + b;
+// }
+// console.log(sum(2,7))
+function isDivisibleBy4And8(a) {
+    return a % 4 === 0 && a % 8 === 0;
+}
+console.log(isDivisibleBy4And8(12));
+console.log(isDivisibleBy4And8(16));
---
+++ index.ts
@@ -0,0 +1,14 @@
+
+// function sum(a:number,b:number):number{
+//     return a + b;
+// }
+
+// console.log(sum(2,7))
+
+
+function isDivisibleBy4And8(a:number):boolean{
+    return a % 4 === 0 && a % 8 === 0 ;
+}
+
+console.log(isDivisibleBy4And8(12))
+console.log(isDivisibleBy4And8(16))
